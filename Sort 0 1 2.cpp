aaa9a1c30cde661420b09f54620a2f319b469759
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2023-06-05 13:31:29 +0530

    Create Sort 0 1 2.cpp

---
+++ Sort 0 1 2.cpp
@@ -0,0 +1,20 @@
+//https://www.codingninjas.com/codestudio/problems/sort-0-1-2_631055?topList=striver-sde-sheet-problems&leftPanelTab=0
+
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   //Write your code
+   int count0 = 0;
+   int count1= 0;
+   int count2 = 0;
+
+   for(int i=0; i<n; i++) {
+      if(arr[i] == 0) count0++;
+      else if(arr[i] == 1) count1++;
+      else count2++;
+   }
+
+   for(int i=0; i<count0; i++) arr[i] = 0;
+   for(int i=count0; i<count0+count1; i++) arr[i] = 1;
+   for(int i=count0+count1; i<n; i++) arr[i] = 2;
+}
