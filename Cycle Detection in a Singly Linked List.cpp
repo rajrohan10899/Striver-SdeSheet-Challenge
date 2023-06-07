2f120890ac79b9147f063454851512e557359497
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2023-06-07 12:36:48 +0530

    Update Cycle Detection in a Singly Linked List.cpp

--- Cycle Detection in a Singly Linked List.cpp
+++ Cycle Detection in a Singly Linked List.cpp
@@ -51,3 +51,36 @@ bool detectCycle(Node *head)
     return true;
 
 }
+
+
+/** LEETCODE QUESTION:
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+        if(head == NULL || head->next == NULL)
+        {
+            return false;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(slow != fast)
+        {
+            if(fast == NULL || fast->next == NULL) 
+            {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true;
+    }
+};
