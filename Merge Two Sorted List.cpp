c42153baebf28569f1435d1c653f070ce39cf067
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2023-06-08 12:32:05 +0530

    Update Merge Two Sorted List.cpp

--- Merge Two Sorted List.cpp
+++ Merge Two Sorted List.cpp
@@ -39,3 +39,59 @@ public:
         return head->next;
     }
 };
+
+// https://www.codingninjas.com/codestudio/problems/800332?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+
+#include <bits/stdc++.h>
+
+/************************************************************
+
+    Following is the linked list node structure.
+    
+    template <typename T>
+    class Node {
+        public:
+        T data;
+        Node* next;
+
+        Node(T data) {
+            next = NULL;
+            this->data = data;
+        }
+
+        ~Node() {
+            if (next != NULL) {
+                delete next;
+            }
+        }
+    };
+
+************************************************************/
+
+Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
+{
+    // Write your code here.
+    Node<int>* head = new Node<int>(0);
+    Node<int>* curr = head;
+
+    while(first != NULL && second != NULL)
+    {
+        if(first->data <= second->data)
+        {
+            curr->next = first;
+            first = first->next;
+        }
+        else {
+            curr->next = second;
+            second = second->next;
+        }
+        curr = curr->next;
+    }
+    if(first != NULL) curr->next = first;
+    
+    else{
+        curr->next = second;
+    }
+    return head->next;
+}
+
