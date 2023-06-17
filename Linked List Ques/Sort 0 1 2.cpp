cf13a17309a02e030c067914d22b9e0333ff13aa
Rohan Raj <rohan_raj2019@manavrachna.net>
2023-06-17 11:35:35 +0530

    Add Files to Folder

--- Linked List Ques/Sort 0 1 2.cpp
+++ Linked List Ques/Sort 0 1 2.cpp
@@ -23,9 +23,9 @@ void sort012(int *arr, int n)
 
 int low = 0, mid = 0, high = n-1, // these are 3 pointers. 
 
-while(mid<=high) {
+while (mid<=high) {
    if(arr[mid] == 0) {
-      swap(arr[mid], arr[low]);
+      swap (arr[mid], arr[low]);
       low++;
       mid++;
    }
