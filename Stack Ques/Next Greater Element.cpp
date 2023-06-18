0da19847ee42be726a3dbb6a49acfb9dba29f6d7
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2023-06-18 07:54:11 +0530

    Update Next Greater Element.cpp

--- Stack Ques/Next Greater Element.cpp
+++ Stack Ques/Next Greater Element.cpp
@@ -20,3 +20,47 @@ vector<int> nextGreaterElement(vector<int>& arr, int n){
     }
     return ans;
 }
+
+// OPTIMAL SOLUTION. USING SATCK
+
+#include <bits/stdc++.h> 
+    
+    vector<int> nextGreaterElement(vector <int>& arr, int n) {
+        // Write your code here.
+    vector<int> ans(n);
+
+    // Stack, to keep track of next greater element.
+    stack<int> s;
+
+     // Traverse in reverse fashion in the array.
+    for (int i = n - 1; i >= 0; i--) {
+
+      /* 
+          If current element is greater than stack's top 
+          element, we can pop it since now it will never get 
+          picked as the next greater, because current element 
+          is greater and appear earlier. 
+      */
+      while (!s.empty() && arr[i] >= s.top()) {
+          s.pop();
+      }
+
+      /*
+          If stack is not empty, stack's top element is 
+          the next greater element, else there is none.
+      */
+      if (!s.empty()) {
+          ans[i] = s.top();
+      } 
+
+      else {
+          ans[i] = -1;
+      }
+
+      // Push current element onto stack.
+      s.push(arr[i]);
+  }
+
+  return ans;
+    
+}
