c7cf25d35d1b6292a42c143f7c9fe4370cd38221
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2023-07-06 14:40:20 +0530

    Create Word Search.cpp

---
+++ Recursion And Backtracking Ques/Word Search.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/word-search/
+
+class Solution {
+public:
+    int m, n;
+    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    bool find(vector<vector<char>>& board, int i, int j, int idx, string &word) {
+        if(idx == word.length()) 
+            return true;
+
+        if(i < 0 || j < 0 || i >= m || j >= n || board[i][j] == '*')
+            return false;
+
+        if(board[i][j] != word[idx])
+            return false;
+
+        char temp = board[i][j];
+        board[i][j] = '*';
+
+        for(auto& dir: directions) {
+            int di = i + dir[0];
+            int dj = j + dir[1];
+
+            if(find(board, di, dj, idx + 1, word))
+                return true;
+        }
+
+        board[i][j] = temp;
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        m = board.size();
+        n = board[0].size();
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(board[i][j] == word[0] && find(board, i, j, 0, word))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
